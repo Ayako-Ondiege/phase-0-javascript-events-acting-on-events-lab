d9dd87ea7ceaa4e3eeeaa2b8a537afa8d7dbff6e
Ayako-Ondiege <james.ouma@student.moringaschool.com>
2024-07-25 13:42:06 +0300

    first commiot

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger=document.getElementById('dodger');
+    let newPosition=dodger.style.left.replace('px', '');
+    let left=parseInt('newPosition', 10);
+    if (left>0) {
+        dodger.style.left= `${left-1}px`;
+    }
+     else {
+        dodger.style.left= `${left=1}px`
+     }
+    }
+
+
+
+function moveDodgerRight() {
+    const dodger=document.getElementById('dodger');
+    let newPosition=dodger.style.left.replace('px', '');
+    let left=parseInt(newPosition, 10);
+    if (left<360) {
+        dodger.style.left= `${left+1}px`;
+    }
+    else {
+        dodger.style.left= `${left=1}px`
+    }
+}
